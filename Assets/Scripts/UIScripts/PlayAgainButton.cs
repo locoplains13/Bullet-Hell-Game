3c74f89bf31b4599906577de1c6f510e938519b0
Angel Roberto Andrade <alroc1234@hotmail.com>
2023-06-25 21:44:12 -0700

    added a play again button to game over screen

---
+++ Assets/Scripts/UIScripts/PlayAgainButton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PlayAgainButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
